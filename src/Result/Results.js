a4293fef64f9a1669c973eb00836af4d6bd6eacb
Shivam Mehta <mehtashivam91@gmail.com>
2018-02-22 12:37:39 +0000

    No Conditional Results (User testing is over :) )

--- src/Result/Results.js
+++ src/Result/Results.js
@@ -3,7 +3,6 @@ import { observer } from "mobx-react"
 import Store from "../Store"
 import Styles from "../UI/UiStyles"
 import ResultBlock from "./ResultBlock"
-import NoResults from "./NoResults"
 
 @observer
 class Results extends Component {
@@ -12,40 +11,21 @@ class Results extends Component {
     return (
       <div style={Styles.results}>
         <header style={Styles.ResultsHeader}>
-          <h2 style={{margin: "16px 0" }}>
+          <h2 style={{ margin: "16px 0" }}>
             {Store.searchText}
           </h2>
-          <p style={{color: "#4D616A" }}> <i><small>Filters applied: </small> <br />
-              Species: Homo_sapiens
+          <p style={{ color: "#4D616A" }}> <i><small>Filters applied: </small> <br />
+            Species: Homo_sapiens
               {Store.alleleToggle
-                ? " - Allele frequency " + Store.allelefreq + "%"
-                : ""}
-              {Store.zygosity !== "" ? " - " + Store.zygosity : ""}{" "}
-              {Store.selectedConsequence !== ""
-                ? " - " + Store.selectedConsequence
-                : ""}
-            </i> </p>
+              ? " - Allele frequency " + Store.allelefreq + "%"
+              : ""}
+            {Store.zygosity !== "" ? " - " + Store.zygosity : ""}{" "}
+            {Store.selectedConsequence !== ""
+              ? " - " + Store.selectedConsequence
+              : ""}
+          </i> </p>
         </header>
-
-        {Store.inputCategory === "Genes" ? (
-          Store.inputType === "Gene Symbol" ? (
-            Store.allelefreq > 5 ? (
-              <ResultBlock />
-            ) : (
-                <NoResults />
-              )
-          ) : Store.inputType === "GO" ? (
-            Store.selectedConsequence === "loss of function" ? (
-              <ResultBlock />
-            ) : (
-                <NoResults />
-              )
-          ) : (
-                <ResultBlock />
-              )
-        ) : (
-            <ResultBlock />
-          )}
+        <ResultBlock />
       </div>
     )
   }
